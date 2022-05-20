c923d873d2cd928abbfaee844622813bf6e8531b
TalMagdish <223maskim@gmail.com>
2022-05-19 22:13:03 -0700

    return

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-// function App() {
-//   return <div>Hello React!</div>;
-// }
-
-// export default App;
-import Game from "./Game";
-import "./style.css";
-
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-
-// const img = new Image(50, 50);
-// img.src = "./assets/tank.png";
-
-//const game = new Game(canvas, ctx);
-
 function App() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //game.run(ctx);
-  
-  window.requestAnimationFrame(draw);
+  return <div>Hello React!</div>;
 }
 
 export default App;
+// import Game from "./Game";
+// import "./style.css";
+
+// const canvas = document.getElementById("myCanvas");
+// const ctx = canvas.getContext("2d");
+
+// // const img = new Image(50, 50);
+// // img.src = "./assets/tank.png";
+
+// //const game = new Game(canvas, ctx);
+
+// function App() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   //game.run(ctx);
+  
+//   window.requestAnimationFrame(draw);
+// }
+
+// export default App;
