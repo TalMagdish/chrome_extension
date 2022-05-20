ce47ff4b034886ce1f01bc351cd79645dc9b4726
TalMagdish <223maskim@gmail.com>
2022-05-20 10:04:54 -0700

    background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from 'react';
+
 function App() {
-  return <div>Hello React!</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: 'blue',
+        width: '100px',
+        height: '100px'
+      }}
+    />
+  );
 }
 
 export default App;
