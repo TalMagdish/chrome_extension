4077765b1974248f3a9f0a89a7f3eac60ada9b44
TalMagdish <223maskim@gmail.com>
2022-05-25 23:20:17 -0700

    done for tonight, next: white margin + font

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
-// function App() {
-//   return <div>Hello React!</div>;
-// }
-// 
-// export default App;
-
 import React from 'react';
 import image from "./img/beach.jpg"; 
 
-function App() {
-  return (
-    // <div
-    //   style={{
-    //     backgroundColor: 'blue',
-    //     backgroundPosition: 'center',
-    //     backgroundSize: 'cover',
-    //     backgroundRepeat: 'no-repeat',
-    //      width: '100vw',
-    //      height: '100vh'
-    //   }}
-    // />
-    <div style={{
-      backgroundImage:`url(${image})`,
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      width: '100vw',
-      height: '100vh'}}>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    curTime: ""
+  };
+ 
+  componentDidMount() {
+    this.getTime();
+  }
+ 
+  getTime = () => {
+    var today = new Date(),
+    //curTime = today.toLocaleString('en-US', { hour: 'numeric', hour12: true }) + ':' + String(today.getMinutes()).padStart(2, '0') + ';'+((today.getHours % 12) || 12) + '....';
+    curTime = today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    this.setState({ curTime });
+  };
+
+  render(){
+    return (
+      <div style={{
+            backgroundImage:`url(${image})`,
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            width: '100vw',
+            height: '100vh'}}
+            className="App">
+        <p>{this.state.curTime}</p>
+      </div>
+    );
+  }
+  // return (
+  //   <div style={{
+  //     backgroundImage:`url(${image})`,
+  //     backgroundPosition: 'center',
+  //     backgroundRepeat: 'no-repeat',
+  //     backgroundSize: 'cover',
+  //     width: '100vw',
+  //     height: '100vh'}}>
+  //   </div>
+  // );
 }
 
 export default App;
