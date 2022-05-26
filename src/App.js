44f68f4e59dfe078825aeaf02f1d61dcc0a05582
TalMagdish <223maskim@gmail.com>
2022-05-25 22:42:49 -0700

    initial backgeound

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@
 // export default App;
 
 import React from 'react';
+import image from "./img/beach.jpg"; 
 
 function App() {
   return (
-    <div
-      style={{
-        backgroundColor: 'blue',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-         width: '100vw',
-         height: '100vh'
-      }}
-    />
+    // <div
+    //   style={{
+    //     backgroundColor: 'blue',
+    //     backgroundPosition: 'center',
+    //     backgroundSize: 'cover',
+    //     backgroundRepeat: 'no-repeat',
+    //      width: '100vw',
+    //      height: '100vh'
+    //   }}
+    // />
+    <div style={{
+      backgroundImage:`url(${image})`,
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      width: '100vw',
+      height: '100vh'}}>
+    </div>
   );
 }
 
